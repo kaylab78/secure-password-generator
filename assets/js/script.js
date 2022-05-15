135af9b3b9ace61f13266bd513e776688483585c
kaylab78 <kaylabackus78@gmail.com>
2022-05-15 18:13:16 -0500

    Used arrays to help choose random characters and generate password

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function generatePassword() {
   // Select the password length
   do {
     var passwordLength = window.prompt("How many characters do you want your password to have?");
-    
+
     // Check that the password length is between 8 - 128 characters
     if (passwordLength < 8 || passwordLength > 128) {
       window.alert("Please choose a value between 8 and 128.");
@@ -15,7 +15,7 @@ function generatePassword() {
     var confirmUpper = window.confirm("Click OK to use uppercase characters in your password.");
     var confirmNumbers = window.confirm("Click OK to use numbers in your password.");
     var confirmSpecial = window.confirm("Click OK to use special characters in your password.");
-  
+
     // If the user answers no for the previous password questions, a message will appear saying they need to choose at least one character type. The prompts will start over.
     if (confirmLower === false && confirmUpper === false && confirmNumbers === false && confirmSpecial === false) {
       window.alert("You must choose at least one character type for your password.");
@@ -23,50 +23,50 @@ function generatePassword() {
   } while (confirmLower === false && confirmUpper === false && confirmNumbers === false && confirmSpecial === false);
 
   // Generate password
-  // List the character options
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numbers = "0123456789";
-  var special = "~`!@#$%^&*()-_=+{}[]|;:',<>.?";
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialCharacters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "}", "[", "]", "|", ";", ":", "<", ">", ".", "?"];
+  var charactersChosen = [];
+  var result = [];
 
   if (confirmLower) {
-    
-  }
-
-  for (var i = 8; i <= passwordLength; i++) {
-    generatePassword = Math.floor(Math.random() * passwordLength);
+    // If the user chose lowercase, include at least one random lowercase letter from the lowercase array
+    charactersChosen = charactersChosen.concat(lowercase)
+    var chooseLower = lowercase[Math.floor(Math.random() * lowercase.length)];
+    result.push(chooseLower);
   };
 
-  //array?
-  // var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  // var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  // var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  // var specialCharacters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "}", "[", "]", "|", ";", ":", "<", ">", ".", "?"];
+  if (confirmUpper) {
 
-  //object?
-  // var characterOptions = [
-  //   {
-  //     lowercase: "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-  //     uppercase: "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-  //     numerical: "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-  //     special: "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "}", "[", "]", "|", ";", ":", "<", ">", ".", "?",
-  //   }
-  // ];
+    charactersChosen = charactersChosen.concat(uppercase)
+    var chooseUpper = uppercase[Math.floor(Math.random() * uppercase.length)];
+    result.push(chooseUpper);
+  };
 
+  if (confirmNumbers) {
+    charactersChosen = charactersChosen.concat(numbers)
+    var chooseNum = numbers[Math.floor(Math.random() * numbers.length)];
+    result.push(chooseNum);
+  };
 
+  if (confirmSpecial) {
+    charactersChosen = charactersChosen.concat(specialCharacters)
+    var chooseSpecial = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+    result.push(chooseSpecial);
+  };
 
+  // Fill in remaining characters to reach the user's desired password length
+  var length = passwordLength - result.length;
 
-  // If the user wants lowercase letters in their password, the computer should include at least one lowercase letter in the password
-  // do {
-  //   charset = "abcdefghijklmnopqrstuvwxyz"
-
-  //   for (var i=0, n=charset.)
-  // } while (confirmLower)
-
+  for (var i = 0; i < length; i++) {
+    var finalChoice = charactersChosen[Math.floor(Math.random() * charactersChosen.length)];
+    result.push(finalChoice);
+  };
 
   // Return the password to be used in other portion of code
-};
-
+  return result.join("");
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
